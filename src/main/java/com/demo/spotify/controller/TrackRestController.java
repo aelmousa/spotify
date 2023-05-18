b50b85139c4fb4c7ff7dd8bbd82282822867abef
aelmousa <aelmousa@alumni.uv.es>
2023-05-18 21:30:04 +0200

    Creación API para la cración de tracks

--- src/main/java/com/demo/spotify/controller/TrackRestController.java
+++ src/main/java/com/demo/spotify/controller/TrackRestController.java
@@ -2,11 +2,11 @@ package com.demo.spotify.controller;
 
 import com.demo.spotify.model.Track;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,8 +17,8 @@ public class TrackRestController {
   public TrackRestController() {
     tracks = new ArrayList<>();
     Track track1 = new Track(1L, "Vuela con el viento", 200L, "Canción moderna");
-    Track track2 = new Track(2L, "Vuela con Alfredo", 200L, "Canción moderna");
-    Track track3 = new Track(3L, "Vuela", 200L, "Canción moderna");
+    Track track2 = new Track(2L, "Vuela con Alfredo", 200L, "Canción moderna 2");
+    Track track3 = new Track(3L, "Vuela", 200L, "Canción moderna 3");
     tracks.add(track1);
     tracks.add(track2);
     tracks.add(track3);
@@ -27,5 +27,10 @@ public class TrackRestController {
   @GetMapping("/tracks")
   public List<Track> getAllTracks(){
     return tracks;
+  }
+
+  @PostMapping("/tracks")
+  public void createTrack(@RequestBody Track track){
+    tracks.add(track);
   }
 }
